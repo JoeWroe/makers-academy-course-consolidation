22a497693e376883aef4e560f604cb7ec186591f
JoeWroe <joe_wroe@icloud.com>
2016-04-06 12:39:41 +0100

    BACK TO THE UNIT
    Added a test for the #DockingStation to respond to release bike.
    Added a release bike method to the #DockingStation class, it currently does nothing however.

--- boris-bikes/spec/docking_station_spec.rb
+++ boris-bikes/spec/docking_station_spec.rb
@@ -2,4 +2,10 @@ require 'docking_station'
 
 describe DockingStation do
 
+  it 'responds to #release_bike' do
+    expect(subject).to respond_to(:release_bike)
+  end
+
+  it { is_expected.to respond_to(:release_bike) }
+
 end
