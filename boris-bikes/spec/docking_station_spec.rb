16bd2ce29b152ed70a85e664d6e5576a7d28b532
JoeWroe <joe_wroe@icloud.com>
2016-04-16 16:55:32 +0100

    Finish chapter 10
    Making Docking Stations Release Bikes
    
    ∆ Add test for #DockingStation.release_bike. This tests to check a
    DockingStation releases a new Bike each time the method is called. Test
    also expects that Bike to be #working?
    ∆ Refactor #DockingStation.release_bike to generate a new instance of
    Bike class each time it is called (with #Bike.new). This is the simplest
    way to ensure the test passes for now.
    ∆ Refactor #Bike.working? to now have a hardcoded return of 'true' each
    time it is called. This is also the simplest solution for passing the
    test.

--- boris-bikes/spec/docking_station_spec.rb
+++ boris-bikes/spec/docking_station_spec.rb
@@ -4,4 +4,10 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:release_bike) }
 
+  it 'can release a bike' do
+    station = DockingStation.new
+    bike    = station.release_bike
+    expect(bike.working?).to eq(true)
+  end
+
 end
