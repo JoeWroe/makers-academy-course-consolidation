0b6ee5a858871bf07119c2d6447731f75685972a
JoeWroe <joe_wroe@icloud.com>
2016-04-17 22:59:53 +0100

    Refactor DockingStation Tests
    ∆ Add subject block that makes 'station = DockingStation.new'. This
    DRY's up code into one line and replaces references to subject with
    'station', for readability.
    
    ∆ Add let block that makes 'bike = Bike.new'. As explained above, this
    DRY's up code and improves readability.

--- boris-bikes/spec/docking_station_spec.rb
+++ boris-bikes/spec/docking_station_spec.rb
@@ -2,22 +2,21 @@ require 'docking_station'
 
 describe DockingStation do
 
+  subject(:station) { described_class.new }
+  let(:bike)        { Bike.new }
+
   it 'can release a bike' do
-    station = DockingStation.new
-    bike    = Bike.new
     station.dock(bike)
     expect(station.release_bike).to eq(bike)
     expect(bike.working?).to eq(true)
   end
 
   it 'can dock a bike' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq(bike)
+    station.dock(bike)
+    expect(station.bike).to eq(bike)
   end
 
   it 'raises an error if there are no bikes' do
-    station = DockingStation.new
-    expect { subject.release_bike }.to raise_error("No bikes in Docking Station.")
+    expect { station.release_bike }.to raise_error("No bikes in Docking Station.")
   end
 end
