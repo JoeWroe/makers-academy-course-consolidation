99d8e766493aeac2dbf7edf543116b3702e29b9f
JoeWroe <joe_wroe@icloud.com>
2016-04-17 17:00:39 +0100

    Refactor #dock in DockingStation class chapter 11
    Using Instance Variables
    
    ∆ Refactor test for #dock to ensure it returns the bike that has been
    docked.
    
    ∆ Refactor #dock in DockingStation class to return the argument it is
    passed. The intention being that the bike that is docked will be
    returned so that it is clear what has been docked.

--- boris-bikes/spec/docking_station_spec.rb
+++ boris-bikes/spec/docking_station_spec.rb
@@ -8,7 +8,10 @@ describe DockingStation do
     expect(bike.working?).to eq(true)
   end
 
-  it {is_expected.to respond_to(:dock).with(1).argument}
+  it 'can dock a bike' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq(bike)
+  end
 
   it {is_expected.to respond_to(:bike)}
 
