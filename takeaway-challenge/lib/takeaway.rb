6f1710c303e45cf6b23082fe06d8035e4ce7684b
JoeWroe <joe_wroe@icloud.com>
2016-05-26 12:40:27 +0100

    Pass test for Takeaway returning a #formatted_menu
    ∆ Takeaway #formatted menu returns a string that will be phased out later.
    The string demonstrates what an interpolation will need to look like when

--- takeaway-challenge/lib/takeaway.rb
+++ takeaway-challenge/lib/takeaway.rb
@@ -1,6 +1,8 @@
 class Takeaway
 
   def formatted_menu
+    # To be replaced with string interpolation when dishes and prices have been done.
+    'Starter..........3'
   end
 
 end
--- takeaway-challenge/spec/takeaway_spec.rb
+++ takeaway-challenge/spec/takeaway_spec.rb
@@ -1,5 +1,23 @@
 require 'takeaway'
 
 describe Takeaway do
-  
+
+  subject(:takeaway) { described_class.new }
+
+  let(:arranged_menu) { 'Starter..........3' }
+
+  describe 'returning a formatted menu' do
+
+    xit 'can obtain a list of dishes' do
+
+    end
+
+    xit 'can obtain a list of prices' do
+
+    end
+
+    it 'can return a menu' do
+      expect(takeaway.formatted_menu).to eq(arranged_menu)
+    end
+  end
 end
