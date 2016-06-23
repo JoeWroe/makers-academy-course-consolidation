bbe3cffd24a90ee31e59ec9243001993d3c18cc3
JoeWroe <joe_wroe@icloud.com>
2016-06-23 17:50:24 +0100

    Add FizzBuzz functionality
    ∆ Add tests and code base to pass FizzBuzz challenge in JavaScript.

--- js-fizz-buzz/jasmine/spec/fizzBuzzSpec.js
+++ js-fizz-buzz/jasmine/spec/fizzBuzzSpec.js
@@ -1,3 +1,59 @@
+describe('FizzBuzz', function() {
+
+  var fizzBuzz;
+
+  beforeEach(function() {
+    fizzBuzz = new FizzBuzz();
+  });
+
+  describe('knows when a number is', function() {
+
+    it('divisible by three', function() {
+      expect(fizzBuzz.isDivisibleByThree(3)).toBe(true);
+    });
+
+    it('divisible by five', function() {
+      expect(fizzBuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
+    it('divisible by both three and five', function() {
+      expect(fizzBuzz.isDivisibleByBothThreeAndFive(15)).toBe(true);
+    });
+
+  });
+
+  describe('knows when a number is NOT', function() {
+
+    it('divisible by three', function() {
+      expect(fizzBuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+    it('divisible by five', function() {
+      expect(fizzBuzz.isDivisibleByFive(1)).toBe(false);
+    });
+
+    it('divisible by both three and five', function() {
+      expect(fizzBuzz.isDivisibleByBothThreeAndFive(1)).toBe(false);
+    });
+  });
+
+  describe('during play, says', function() {
+
+    it('"Fizz" when number is divisible by three', function() {
+      expect(fizzBuzz.says(3)).toEqual('Fizz');
+    });
+
+    it('"Buzz" when number is divisible by five', function() {
+      expect(fizzBuzz.says(5)).toEqual('Buzz');
+    });
+
+    it('"FizzBuzz" when number is divisible by both three and five', function() {
+      expect(fizzBuzz.says(15)).toEqual('FizzBuzz');
+    });
+  });
+});
+
+
 // describe("Player", function() {
 //   var player;
 //   var song;
