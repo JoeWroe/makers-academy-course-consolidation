e029d96b61642b8e790de40db6e6403ac6fbc36e
JoeWroe <joe_wroe@icloud.com>
2016-06-23 18:58:27 +0100

    Clean up spec and src

--- js-fizz-buzz/jasmine/spec/fizzBuzzSpec.js
+++ js-fizz-buzz/jasmine/spec/fizzBuzzSpec.js
@@ -9,15 +9,15 @@ describe('FizzBuzz', function() {
   describe('knows when a number is', function() {
 
     it('divisible by three', function() {
-      expect(fizzBuzz.isDivisibleByThree(3)).toBe(true);
+      expect(fizzBuzz._isDivisibleByThree(3)).toBe(true);
     });
 
     it('divisible by five', function() {
-      expect(fizzBuzz.isDivisibleByFive(5)).toBe(true);
+      expect(fizzBuzz._isDivisibleByFive(5)).toBe(true);
     });
 
     it('divisible by both three and five', function() {
-      expect(fizzBuzz.isDivisibleByBothThreeAndFive(15)).toBe(true);
+      expect(fizzBuzz._isDivisibleByBothThreeAndFive(15)).toBe(true);
     });
 
   });
@@ -25,15 +25,15 @@ describe('FizzBuzz', function() {
   describe('knows when a number is NOT', function() {
 
     it('divisible by three', function() {
-      expect(fizzBuzz.isDivisibleByThree(1)).toBe(false);
+      expect(fizzBuzz._isDivisibleByThree(1)).toBe(false);
     });
 
     it('divisible by five', function() {
-      expect(fizzBuzz.isDivisibleByFive(1)).toBe(false);
+      expect(fizzBuzz._isDivisibleByFive(1)).toBe(false);
     });
 
     it('divisible by both three and five', function() {
-      expect(fizzBuzz.isDivisibleByBothThreeAndFive(1)).toBe(false);
+      expect(fizzBuzz._isDivisibleByBothThreeAndFive(1)).toBe(false);
     });
   });
 
@@ -52,63 +52,3 @@ describe('FizzBuzz', function() {
     });
   });
 });
-
-
-// describe("Player", function() {
-//   var player;
-//   var song;
-//
-//   beforeEach(function() {
-//     player = new Player();
-//     song = new Song();
-//   });
-//
-//   it("should be able to play a Song", function() {
-//     player.play(song);
-//     expect(player.currentlyPlayingSong).toEqual(song);
-//
-//     //demonstrates use of custom matcher
-//     expect(player).toBePlaying(song);
-//   });
-//
-//   describe("when song has been paused", function() {
-//     beforeEach(function() {
-//       player.play(song);
-//       player.pause();
-//     });
-//
-//     it("should indicate that the song is currently paused", function() {
-//       expect(player.isPlaying).toBeFalsy();
-//
-//       // demonstrates use of 'not' with a custom matcher
-//       expect(player).not.toBePlaying(song);
-//     });
-//
-//     it("should be possible to resume", function() {
-//       player.resume();
-//       expect(player.isPlaying).toBeTruthy();
-//       expect(player.currentlyPlayingSong).toEqual(song);
-//     });
-//   });
-//
-//   // demonstrates use of spies to intercept and test method calls
-//   it("tells the current song if the user has made it a favorite", function() {
-//     spyOn(song, 'persistFavoriteStatus');
-//
-//     player.play(song);
-//     player.makeFavorite();
-//
-//     expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-//   });
-//
-//   //demonstrates use of expected exceptions
-//   describe("#resume", function() {
-//     it("should throw an exception if song is already playing", function() {
-//       player.play(song);
-//
-//       expect(function() {
-//         player.resume();
-//       }).toThrowError("song is already playing");
-//     });
-//   });
-// });
--- js-fizz-buzz/jasmine/src/fizzBuzz.js
+++ js-fizz-buzz/jasmine/src/fizzBuzz.js
@@ -1,53 +1,32 @@
 var FizzBuzz = function() {};
 
-FizzBuzz.prototype.isDivisibleByThree = function(number) {
-  return this._isDivisibleBy(number, 3);
-};
-
-FizzBuzz.prototype.isDivisibleByFive = function(number) {
-  return this._isDivisibleBy(number, 5);
-};
-
-FizzBuzz.prototype.isDivisibleByBothThreeAndFive = function(number) {
-  return this._isDivisibleBy(number, 15);
-};
-
 FizzBuzz.prototype.says = function(number) {
-  if (this.isDivisibleByBothThreeAndFive(number)) {
+  if (this._isDivisibleByBothThreeAndFive(number)) {
     return "FizzBuzz";
   }
-  if (this.isDivisibleByThree(number)) {
+  if (this._isDivisibleByThree(number)) {
     return "Fizz";
   }
-  if (this.isDivisibleByFive(number)) {
+  if (this._isDivisibleByFive(number)) {
     return "Buzz";
   }
   return number;
 };
 
+// private
+
 FizzBuzz.prototype._isDivisibleBy = function(number, divisor) {
   return(number % divisor === 0);
 };
 
-// function Player() {
-// }
-// Player.prototype.play = function(song) {
-//   this.currentlyPlayingSong = song;
-//   this.isPlaying = true;
-// };
-//
-// Player.prototype.pause = function() {
-//   this.isPlaying = false;
-// };
-//
-// Player.prototype.resume = function() {
-//   if (this.isPlaying) {
-//     throw new Error("song is already playing");
-//   }
-//
-//   this.isPlaying = true;
-// };
-//
-// Player.prototype.makeFavorite = function() {
-//   this.currentlyPlayingSong.persistFavoriteStatus(true);
-// };
+FizzBuzz.prototype._isDivisibleByThree = function(number) {
+  return this._isDivisibleBy(number, 3);
+};
+
+FizzBuzz.prototype._isDivisibleByFive = function(number) {
+  return this._isDivisibleBy(number, 5);
+};
+
+FizzBuzz.prototype._isDivisibleByBothThreeAndFive = function(number) {
+  return this._isDivisibleBy(number, 15);
+};
