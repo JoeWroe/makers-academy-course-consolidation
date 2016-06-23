a9ee3861ef8165790b3930c1ba15eb5508cc96c2
JoeWroe <joe_wroe@icloud.com>
2016-06-23 23:47:11 +0100

    Add temperature increase and decrease functionality
    ∆ Add #increaseTemperature. Given arg, increases #currentTemperature
    by the value passed in at the arg.
    
    ∆ Add #decreaseTemperature. Given arg, decreases #currentTemperature
    by the value passed in at the arg.

--- js-thermostat/spec/thermostatSpec.js
+++ js-thermostat/spec/thermostatSpec.js
@@ -4,12 +4,26 @@ describe('Thermostat', function() {
 
   beforeEach(function() {
     thermostat = new Thermostat();
+    thermostat.initializeThermostat();
   });
 
   describe('when initialized', function() {
 
     it('sets the temperature to 20 degrees', function() {
-      expect(thermostat.currentTemperature()).toEqual(20);
+      expect(currentTemperature).toEqual(20);
+    });
+  });
+
+  describe('changing the temperature', function() {
+
+    it('can be increased', function() {
+      thermostat.increaseTemperature(1);
+      expect(currentTemperature).toEqual(21);
+    });
+
+    it('can be decreased', function() {
+      thermostat.decreaseTemperature(1);
+      expect(currentTemperature).toEqual(19);
     });
   });
 });
--- js-thermostat/src/thermostat.js
+++ js-thermostat/src/thermostat.js
@@ -1,5 +1,22 @@
 var Thermostat = function() {};
 
-Thermostat.prototype.currentTemperature = function() {
-  return 20;
+var currentTemperature;
+
+Thermostat.prototype.initializeThermostat = function() {
+  this._updateCurrentTemperature(20);
+  return currentTemperature;
+};
+
+Thermostat.prototype.increaseTemperature = function(increaseValue) {
+  currentTemperature += increaseValue;
+};
+
+Thermostat.prototype.decreaseTemperature = function(decreaseValue) {
+  currentTemperature -= decreaseValue;
+};
+
+// private methods
+
+Thermostat.prototype._updateCurrentTemperature = function(tempValue) {
+  currentTemperature = tempValue;
 };
