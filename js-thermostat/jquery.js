f60826a003dbe40d61f48b90ecfc3f0c731e857a
JoeWroe <joe_wroe@icloud.com>
2016-06-26 13:52:52 +0100

    Add jQuery to buttons
    ∆ Add jQuery to connect buttons with JavaScript. This adds functionality
    to page that allows temperature increase and decrease, reset temperature
    and power saving.

--- js-thermostat/jquery.js
+++ js-thermostat/jquery.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
 
+  var thermostat = new Thermostat();
+
+  $('#current-temperature').text(thermostat.currentTemperature);
+
+  $('#power-saving').text(thermostat.powerSaving);
+
+  $('#increase-temperature').on('click', function() {
+    thermostat.increaseTemperature(1);
+    $('#current-temperature').text(thermostat.currentTemperature);
+  });
+
+  $('#decrease-temperature').on('click', function() {
+    thermostat.decreaseTemperature(1);
+    $('#current-temperature').text(thermostat.currentTemperature);
+  });
+
+  $('#reset-temperature').on('click', function() {
+    thermostat.resetTemperature();
+    $('#current-temperature').text(thermostat.currentTemperature);
+  });
+
+  $('#toggle-power-saving').on('click', function() {
+    thermostat.togglePowerSaving();
+    $('#power-saving').text(thermostat.powerSaving);
+  });
+
   $('#power-saving-status').fadeTo(1, 0.95);
 
   $('#power-saving-status').mouseenter(function() {
