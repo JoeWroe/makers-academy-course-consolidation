93ebb529fbb1ea09c95738fef39e0d951618351e
JoeWroe <joe_wroe@icloud.com>
2016-06-26 12:38:37 +0100

    Style #current-temperature
    ∆ Using style.css add position and colour to div #current-temperature.

--- js-thermostat/jquery.js
+++ js-thermostat/jquery.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
-  $('#power-saving-status').fadeTo(1, 0.8);
+  $('#power-saving-status').fadeTo(1, 0.95);
 
   $('#power-saving-status').mouseenter(function() {
     $('#power-saving-status').fadeTo('fast', 1);
   });
 
   $('#power-saving-status').mouseleave(function() {
-    $('#power-saving-status').fadeTo('fast', 0.8);
+    $('#power-saving-status').fadeTo('fast', 0.95);
   });
 });
